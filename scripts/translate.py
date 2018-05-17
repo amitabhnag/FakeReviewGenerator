e31cb590a2e63116d4f4b116a5b428594c3f7a06
gautam <mgautam@uw.edu>
2018-05-17 02:12:45 -0700

    added file for google translate

---
+++ scripts/translate.py
@@ -0,0 +1,26 @@
+"""This module translates text passed to it as a parameter, into Chinese and back to English.
+This is a post processing step to smoothen the output.
+
+Prerequisites:
+--------------
+pip install googletrans
+
+Function:
+--------
+translate()
+
+Returns:
+--------
+translated string in english
+
+"""
+
+from googletrans import Translator
+
+def translate(word):
+	translator = Translator()
+	chin_text = translator.translate(word,src='en',dest='zh-cn')
+	#print(chin_text.text)
+	eng_text = translator.translate(chin_text.text,src='zh-cn',dest='en')
+	#print(eng_text.text)
+	return eng_text.text
