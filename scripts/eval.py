860e85a2e76e20d25f9d4ceef8a880160af3be50
Toan Luong <toan503@gmail.com>
2018-05-22 00:38:26 -0700

    Raise errors for input conditions

--- scripts/eval.py
+++ scripts/eval.py
@@ -1,18 +1,25 @@
 import language_check
 import random
+import os
 
 def eval_txt(file_path, verbose=False):
-	with open('tn_output_raw.txt', 'r') as f: 
-		data_raw = f.read().replace('\n', '')
-	tool = language_check.LanguageTool('en-US')
-	matches = tool.check(data_raw)
-	if verbose:
-		print(matches[random.randint(0, len(matches)-1)])
-	return len(matches)
+	if not os.path.exists(file_path):
+		raise OSError("File Not Found")
+	else:
+		with open(file_path, 'r') as f: 
+			data_raw = f.read().replace('\n', '')
+		tool = language_check.LanguageTool('en-US')
+		matches = tool.check(data_raw)
+		if verbose and len(matches) > 0:
+			print(matches[random.randint(0, len(matches)-1)])
+		return len(matches)
 
 def eval_str(output, verbose=False):
-	tool = language_check.LanguageTool('en-US')
-	matches = tool.check(output)
-	if verbose:
-		print(matches[random.randint(0, len(matches)-1)])
-	return len(matches)
+	if len(output) == 0:
+		raise ValueError("File is empty")
+	else:
+		tool = language_check.LanguageTool('en-US')
+		matches = tool.check(output)
+		if verbose and len(matches) > 0:
+			print(random.choice(matches))
+		return len(matches)
