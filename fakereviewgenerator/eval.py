efbb1a10edaead5ee56caca3d2c14fe0fcc0f29d
Amitabh Nag <amitabh_nag1@yahoo.com>
2018-05-27 13:03:24 -0700

    PEP08 - added docstring to eval.py

--- fakereviewgenerator/eval.py
+++ fakereviewgenerator/eval.py
@@ -1,8 +1,27 @@
-import language_check
+""" This module performs grammer check on a text file content
+or a string. This check allows an objective evaluation of
+sampled text from the model
+Functions:
+eval_txt:This function reads a text file and performs
+    grammer check on the text
+eval_str: This function take string as a parameter and performs
+    grammer check on it
+"""
 import random
 import os
+import language_check
 
 def eval_txt(file_path, verbose=False):
+    """ This function reads a text file and performs
+    grammer check on the text
+    Parameters:
+        file_path: Complete path to the text file
+        verbose: Verbose output (False)
+    Return Value:
+        Number of grammer errors, error details
+    Exceptions:
+        OSError: Raised if the text file is not found
+    """
     if not os.path.exists(file_path):
         raise OSError("File Not Found")
     else:
@@ -15,6 +34,14 @@ def eval_txt(file_path, verbose=False):
         return len(matches), matches
 
 def eval_str(output, verbose=False):
+    """ This function take string as a parameter and performs
+    grammer check on it
+    Parameters:
+        output: String to run grammer check on
+        verbose: Verbose output (False)
+    Return Value:
+        Number of grammer errors, error details
+    """
     if len(output) == 0:
         raise ValueError("File is empty")
     else:
